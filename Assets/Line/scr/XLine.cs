e9a413548ed6aa88ae01229621db4ec12ff3551d
lsr1177 <91503885+lsr1177@users.noreply.github.com>
2022-10-14 10:49:12 +0200

    Create XLine.cs

---
+++ Assets/Line/scr/XLine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XLine : MonoBehaviour {
+	public GameObject Line;
+	public GameObject FXef;//Effet de particules du laser frappant l'objet
+
+    // Use this for initialization
+    // Update is called once per frame
+    void Update () {
+		RaycastHit hit;
+		Vector3 Sc;// Transformer la taille
+		Sc.x=0.5f;
+		Sc.z=0.5f;
+        if (Physics.Raycast(transform.position, this.transform.forward, out hit)){
+			Debug.DrawLine(this.transform.position,hit.point);
+			Sc.y=hit.distance;
+			FXef.transform.position=hit.point;
+			FXef.SetActive(true);
+		}
+        //Lorsque le laser ne touche pas l'objet, maintenez la longueur du rayon à 500 m et réglez l'effet de frappe pour qu'il ne s'affiche pas
+        else
+        {
+			Sc.y=500;
+		    FXef.SetActive(false);
+		}
+			
+		Line.transform.localScale=Sc;
+            
+	}
+}
