42145e2d18d12decae1561e00bd66d17472f5d5c
lsr1177 <91503885+lsr1177@users.noreply.github.com>
2022-10-14 16:51:33 +0200

    Update Door_Disappear.cs

--- Assets/Scripts/Door_Disappear.cs
+++ Assets/Scripts/Door_Disappear.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Door_Disappear : MonoBehaviour
 {
     public GameObject Statue;
-    Vector3 v1 = new Vector3(0, 0.6799998f, 0); // the postion of "Normand" on the map
-    //bool existence = true;
+    Vector3 v1 = new Vector3(-1.91f, 0.8382025f, 29.14f); // the postion of "Normand" on the map
+    bool closed = true;
     public AudioSource music;
     public AudioClip Open_door;
+    private Door m_LeftDoor;
+    private Door m_RightDoor;
+    public GameObject LeftDoorShaft;
+    public GameObject RightDoorShaft;
+    public GameObject Collider;
 
     public bool Open(GameObject Statue)
     {
@@ -33,18 +39,37 @@ public class Door_Disappear : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_LeftDoor = LeftDoorShaft.GetComponent<Door>();
+        m_RightDoor = RightDoorShaft.GetComponent<Door>();
     }
 
     // Update is called once per frame
     void Update()
-    {   
-         if (Open(Statue) == true)
-         {
-            Destroy(this.gameObject);
-            music.clip = Open_door;
-            music.Play();
-         }
+    {   if (closed)
+        {
+            float LeftDoorShaftRotation = LeftDoorShaft.transform.localEulerAngles.y;
+            float RightDoorShaftRotation = RightDoorShaft.transform.localEulerAngles.y;
+            Debug.Log(LeftDoorShaftRotation);
+            Debug.Log(closed);
+            if ((LeftDoorShaftRotation >= 100) && (-180 <= RightDoorShaftRotation))
+            {
+                closed = false;
+            }
+            if (Open(Statue) == true)
+            {
+                Destroy(Collider);
+
+                music.clip = Open_door;
+                music.Play();
+
+                
+                m_LeftDoor.OpenLeftDoorMethod();
+                m_RightDoor.OpenRightDoorMethod();
+
+            }
+
+        }
+         
         
 
     }
