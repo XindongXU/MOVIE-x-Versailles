32e28a1ca4ccc67154136314b1fa0bbba0630c00
Arthur RICHE <arthur.riche@mines-paristech.fr>
2022-10-24 16:47:03 +0200

    Statue SCRIPT_CLICK

---
+++ Assets/Scripts/SCRIPT_CLICK.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+using UnityEngine.Events;
+
+public class SCRIPT_CLICK : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+    public UnityEvent EffectToDo;
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        EffectToDo?.Invoke();
+
+        
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
