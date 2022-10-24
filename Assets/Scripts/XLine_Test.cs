6f97b44afd8a9fafdadb42b405b1aaf42b945bc5
Arthur RICHE <arthur.riche@mines-paristech.fr>
2022-10-24 10:29:56 +0200

    Test Laser

---
+++ Assets/Scripts/XLine_Test.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+
+
+public class XLine_Test : MonoBehaviour {
+	public GameObject Line;
+	public GameObject FXef;//Effet de particules du laser frappant l'objet
+
+	bool laser = false;
+
+    
+	public void DoEffect()
+    {
+    
+    
+
+    // Use this for initialization
+    // Update is called once per frame
+
+		RaycastHit hit;
+		Vector3 Sc;// Transformer la taille
+		Sc.x=0.5f;
+		Sc.z=0.5f;
+        if (Physics.Raycast(transform.position, this.transform.forward, out hit)){
+			Debug.DrawLine(this.transform.position,hit.point);
+			Sc.y=hit.distance;
+			FXef.transform.position=hit.point;
+			FXef.SetActive(true);
+		}
+        //Lorsque le laser ne touche pas l'objet, maintenez la longueur du rayon à 500 m et réglez l'effet de frappe pour qu'il ne s'affiche pas
+        else
+        {
+			Sc.y=500;
+		    FXef.SetActive(false);
+		}
+			
+		Line.transform.localScale=Sc;
+        bool laser = true;
+	}
+
+    public bool GetLaser()
+        {
+            return laser;
+        }
+
+}
