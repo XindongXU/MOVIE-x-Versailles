247ab1f648747cd36652592e67cc0ff9fbda0bcb
XindongXU <xxd_ars@sjtu.edu.cn>
2022-10-25 15:00:07 +0200

    diams l

--- Assets/Scripts/SCRIPT_CLICK.cs
+++ Assets/Scripts/SCRIPT_CLICK.cs
@@ -16,13 +16,39 @@ public class SCRIPT_CLICK : MonoBehaviour
         laserPointer.PointerIn += PointerInside;
         laserPointer.PointerOut += PointerOutside;
         laserPointer.PointerClick += PointerClick;
+        laserPointer.PointerClick += PointerClick2;
+        laserPointer.PointerClick += PointerClick3;
     }
-    public UnityEvent EffectToDo;
+    public UnityEvent EffectToDo1;
     public void PointerClick(object sender, PointerEventArgs e)
     {
-        EffectToDo?.Invoke();
+        if (e.target.name == "statue1_venus_d2_lod1_100k_t1_8k_Material_u1_v1.001")
+        {
+            EffectToDo1?.Invoke();
+        }
 
-        
+        if (e.target.name == "statue2_venus_d2_lod1_100k_t1_8k_Material_u1_v1.001")
+        {
+            EffectToDo1?.Invoke();
+        }
+    }
+    public UnityEvent EffectToDo2;
+    public void PointerClick2(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "DiamandVert")
+        {
+            EffectToDo2?.Invoke();
+        }
+    }
+
+    public UnityEvent EffectToDo3;
+    public void PointerClick3(object sender, PointerEventArgs e)
+    { 
+        if (e.target.name == "DiamandBleu")
+        {
+            EffectToDo3?.Invoke();
+        }
+    
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
