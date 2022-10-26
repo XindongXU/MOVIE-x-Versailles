7ee814f251a3e5a9cc3fe82c02173fec9527ee07
Arthur RICHE <arthur.riche@mines-paristech.fr>
2022-10-26 16:02:49 +0200

    ygu

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] TMPro.TextMeshProUGUI countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString();
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+
+        }
+    }
+}
