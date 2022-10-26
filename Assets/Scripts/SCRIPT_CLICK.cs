25ee580307c26a2acebcfc423dffb6d8e184ac38
Arthur RICHE <arthur.riche@mines-paristech.fr>
2022-10-26 09:27:48 +0200

    Update SCRIPT CLICK

--- Assets/Scripts/SCRIPT_CLICK.cs
+++ Assets/Scripts/SCRIPT_CLICK.cs
@@ -15,22 +15,46 @@ public class SCRIPT_CLICK : MonoBehaviour
     {
         laserPointer.PointerIn += PointerInside;
         laserPointer.PointerOut += PointerOutside;
-        laserPointer.PointerClick += PointerClic1;
+        laserPointer.PointerClick += PointerClick;
         laserPointer.PointerClick += PointerClick2;
         laserPointer.PointerClick += PointerClick3;
     }
     public UnityEvent EffectToDo1;
-    public void PointerClick1(object sender, PointerEventArgs e)
+    public void PointerClick(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "Cube")
+        if (e.target.name == "statue1_venus_d2_lod1_100k_t1_8k_Material_u1_v1.001")
+        {
+            EffectToDo1?.Invoke(); 
+        }
+
+        if (e.target.name == "statue2_venus_d2_lod1_100k_t1_8k_Material_u1_v1.001")
         {
             EffectToDo1?.Invoke(); 
         }
     }
 
+    public UnityEvent EffectToDo2;
+    public void PointerClick2(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "DiamandBleu")
+        {
+            EffectToDo2?.Invoke(); 
+        }
+    }
+
+    public UnityEvent EffectToDo3;
+    public void PointerClick3(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "DiamandVert")
+        {
+            EffectToDo3?.Invoke(); 
+        }
+    }
+
+
     public void PointerInside(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "Cube")
+        if (e.target.name == "")
         {
             Debug.Log("Cube was entered");
         }
