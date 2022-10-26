80bdc3bd4869f8656667c2c051c3e0ba0803f473
XindongXU <xxd_ars@sjtu.edu.cn>
2022-10-26 15:46:29 +0200

    00

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -5,45 +5,38 @@ using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
 {
-    private Text txtTimer;
+    float currentTime = 0f;
+    public float startingTime;
     public Image _GOImage;
-    public float second;
- 
-    private void Start()
-    {
-        txtTimer = this.GetComponent<Text>();
-        // 重复调用（被执行的方法名称，第一次执行时间，每次执行间隔时间）
-        InvokeRepeating("Timer", 1, 1);
-    }
- 
-    private void Timer()
-    {
-        second = second - 1;
-        txtTimer.text = string.Format("{0:d2}:{1:d2}", (int)second / 60, (int)second % 60);
-        Debug.Log((int)second);
-        Debug.Log(txtTimer.text);
 
-        if (second <= 3)
-        {
-            txtTimer.color = Color.red;
-            if (second <= 0)
-            {
-                if (_GOImage.enabled == false)
-                {
-                    GameOver();
-                }
-                CancelInvoke("Timer");
-            }
-        }
+    [SerializeField] TMPro.TextMeshProUGUI countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
     }
 
     private void GameOver()
     {
         _GOImage.enabled = true;
+
     }
 
-    private void Update()
+    void Update()
     {
+        currentTime -= 1 * Time.deltaTime;
+        // countdownText.text = currentTime.ToString("0");
 
+        countdownText.text = string.Format("{0:d2}:{1:d2}", (int)currentTime / 60, (int)currentTime % 60);
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            if (_GOImage.enabled == false)
+            {
+                GameOver();
+            }
+
+        }
     }
- }
+}
