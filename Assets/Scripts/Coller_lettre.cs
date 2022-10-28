fa4e7c855218491d72a0974a24d014d594a00221
lsr1177 <91503885+lsr1177@users.noreply.github.com>
2022-10-28 10:01:39 +0200

    letter 2

--- Assets/Scripts/Coller_lettre.cs
+++ Assets/Scripts/Coller_lettre.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class Coller_lettre : MonoBehaviour
 {
-    public Transform B;   //A要跟随的B
-    public float smoothTime = 0.01f;  //A平滑移动的时间
-    private Vector3 AVelocity = Vector3.zero;
-    //private A mainA;  
+    public GameObject B;   //A要跟随的B
+    public GameObject A;
 
-    void Awake()
+    void Start()
     {
-        //mainA = A.main;
+        
     } 
      
     void Update()
     {
-        //transform.position = Vector3.SmoothDamp(transform.position, B.position + new Vector3(0, 0, -5), ref AVelocity, smoothTime);
+        A.transform.position = Vector3.MoveTowards(A.transform.position, B.transform.position, 0.01f);
     }
         
 }
