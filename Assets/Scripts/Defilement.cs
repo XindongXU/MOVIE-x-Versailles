221cdacb4445262960712ac09e24671ef6b010cd
unknown <xxd_ars@sjtu.edu.cn>
2022-10-28 14:13:52 +0200

    bienvenu update

--- Assets/Scripts/Defilement.cs
+++ Assets/Scripts/Defilement.cs
@@ -7,19 +7,29 @@ using Valve.VR.Extras;
 
 public class Defilement : MonoBehaviour
 {
+    float currentTime = 0f;
+    public float startingTime;
     public SteamVR_LaserPointer laserPointer;
     public Image _StartImage;
     public Image _GOImage;
-    public GameObject _BvInterface;
+    // public GameObject _BvInterface;
     public GameObject _BvText;
-    // [SerializeField] TMPro.TextMeshProUGUI countdownText;
+    [SerializeField] TMPro.TextMeshProUGUI countdownText;
 
     void Awake()
     {
-        laserPointer.PointerIn += PointerInside;
-        laserPointer.PointerOut += PointerOutside;
+        // laserPointer.PointerIn += PointerInside;
+        // laserPointer.PointerOut += PointerOutside;
         laserPointer.PointerClick += PointerClick;
 
+    }
+
+    private void GameOver()
+    {
+        if (_GOImage.enabled == false)
+        {
+            _GOImage.enabled = true;
+        }
     }
 
     public void PointerClick(object sender, PointerEventArgs e)
@@ -38,35 +48,43 @@ public class Defilement : MonoBehaviour
             // duration : 120 deltaTime;
             // Destroy(_BvInterface, 10.0f);
             }
-
-        }
-        else if (e.target.name == "BackButton" && _StartImage.enabled == true)
-        {
-            Debug.Log("BackButton was clicked");
             _StartImage.enabled = false;
-            _buttonImage.enabled = false;
+            Destroy(_BvText);
 
+            while(currentTime >= 0)
+            {
+                currentTime -= 1 * Time.deltaTime;
+                countdownText.text = string.Format("{0:d2}:{1:d2}", (int)currentTime / 60, (int)currentTime % 60);
+            }
+            GameOver();
         }
+        // else if (e.target.name == "BackButton" && _StartImage.enabled == true)
+        // {
+        //     Debug.Log("BackButton was clicked");
+        //     _StartImage.enabled = false;
+        //     _buttonImage.enabled = false;
+
+        // }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(this.GetComponent<Transform>().position.y <= 75)
-        {
-            transform.Translate(new Vector3(0, 1 * Time.deltaTime, 0));
-            // from -45 to 75;
-            // duration : 120 deltaTime;
-            // Destroy(_BvInterface, 10.0f);
-        }
-        // Destroy(gameObject);
-        Destroy(_BvInterface);
+        // while(this.GetComponent<Transform>().position.y <= 75)
+        // {
+        //     transform.Translate(new Vector3(0, 1 * Time.deltaTime, 0));
+        //     // from -45 to 75;
+        //     // duration : 120 deltaTime;
+        //     // Destroy(_BvInterface, 10.0f);
+        // }
+        // // Destroy(gameObject);
+        // Destroy(_BvInterface);
 
         
 
