e28031f473fd590b105b0b6b71d2aa867b9bc8cd
unknown <xxd_ars@sjtu.edu.cn>
2022-11-02 16:58:32 +0100

    Ending

--- Assets/Scripts/Is_successful.cs
+++ Assets/Scripts/Is_successful.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Valve.VR.Extras;
 
 public class Is_successful : MonoBehaviour
 {
     public string TagFilter;
+    public Image _EndImage;
+    public GameObject _EndingText;
 
+    bool _IsSuccessful = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +24,17 @@ public class Is_successful : MonoBehaviour
         if (other.name == TagFilter)
         {
             print(this.name + " declench?par " + this.gameObject);
+
             if (other.GetComponent<MeshRenderer>().material.color.a == 0)
             {
                 // XXD
+                _IsSuccessful = true;
+
+                if (_EndImage.enabled == false)
+                {
+                    _EndImage.enabled = true;
+                    Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
+                }
             }
 
         }
@@ -32,6 +47,14 @@ public class Is_successful : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_IsSuccessful == true)
+        {
+            if (_EndingText.GetComponent<Transform>().position.y <= 6)
+            {
+                _EndingText.GetComponent<Transform>().Translate(new Vector3(0, Time.deltaTime/4, 0));
+                Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
+            }
         
+        }
     }
 }
