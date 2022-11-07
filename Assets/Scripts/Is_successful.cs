87c82935237e975c66d5205fa077865d59b8fcd2
XindongXU <xxd_ars@sjtu.edu.cn>
2022-11-07 13:38:29 +0100

    Update Is_successful.cs

--- Assets/Scripts/Is_successful.cs
+++ Assets/Scripts/Is_successful.cs
@@ -16,43 +16,20 @@ public class Is_successful : MonoBehaviour
     [SerializeField] TMPro.TextMeshProUGUI EndingText;
 
     bool _IsSuccessful = false;
+    public string TagFilter;
     // Start is called before the first frame update
     void Start()
     {
         _IsSuccessful = false;
     }
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.name == TagFilter)
-    //    {
-    //        print(this.name + " declench?par " + this.gameObject);
-
-    //        if (other.GetComponent<MeshRenderer>().material.color.a == 0)
-    //        {
-    //            // XXD
-    //            _IsSuccessful = true;
-
-    //            if (_EndImage.enabled == false)
-    //            {
-    //                _EndImage.enabled = true;
-    //                EndingText.enabled = true;
-    //                Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
-    //            }
-    //        }
-    //    }
-    //    else
-    //    {
-    //        print(this.name + " a dt?une collision par un objet non autoris? seul les objets possant le tag : " + TagFilter + " sont autoris?");
-    //    }
-    //}
-
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        if (_IsSuccessful == false)
+        if (other.name == TagFilter)
         {
-            if ((_FinalProve.GetComponent<MeshRenderer>().material.color.a == 0) && (_FinalProve.transform.localPosition.z <= 40) && (_FinalProve.transform.localPosition.z >= 25))
+            print(this.name + " declench?par " + this.gameObject);
+
+            if (other.GetComponent<MeshRenderer>().material.color.a == 0)
             {
                 // XXD
                 _IsSuccessful = true;
@@ -67,20 +44,44 @@ public class Is_successful : MonoBehaviour
         }
         else
         {
-            if (_EndingText.GetComponent<Transform>().position.y <= 5)
-            {
-                _EndingText.GetComponent<Transform>().Translate(new Vector3(0, Time.deltaTime/8, 0));
-                Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
-            }
-            else
-            {
-                _IsSuccessful = false;
-                _EndImage.enabled = false;
-                EndingText.enabled = false;
-                Destroy(_Player);
-                SceneManager.LoadScene(0);
-            }    
-        
+            print(this.name + " a dt?une collision par un objet non autoris? seul les objets possant le tag : " + TagFilter + " sont autoris?");
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (_IsSuccessful == false)
+        //{
+        //    if ((_FinalProve.GetComponent<MeshRenderer>().material.color.a == 0) && (_FinalProve.transform.localPosition.z <= 40) && (_FinalProve.transform.localPosition.z >= 25))
+        //    {
+        //        // XXD
+        //        _IsSuccessful = true;
+
+        //        if (_EndImage.enabled == false)
+        //        {
+        //            _EndImage.enabled = true;
+        //            EndingText.enabled = true;
+        //            Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    if (_EndingText.GetComponent<Transform>().position.y <= 5)
+        //    {
+        //        _EndingText.GetComponent<Transform>().Translate(new Vector3(0, Time.deltaTime/8, 0));
+        //        Debug.Log((int)_EndingText.GetComponent<Transform>().position.y);
+        //    }
+        //    else
+        //    {
+        //        _IsSuccessful = false;
+        //        _EndImage.enabled = false;
+        //        EndingText.enabled = false;
+        //        Destroy(_Player);
+        //        SceneManager.LoadScene(0);
+        //    }    
+        
+        //}
     }
 }
